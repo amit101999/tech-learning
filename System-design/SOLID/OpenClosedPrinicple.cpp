a9262476ac081756c3ff97fd9d622cb4eac91e91
amit from linux machine <amitthakur10sep@gmail.com>
2025-12-08 23:39:17 +0530

    Open closed principle - System Design

---
+++ System-design/SOLID/OpenClosedPrinicple.cpp
@@ -0,0 +1,3 @@
+// IN Open closed principle we dont define all the operations in the single class instead we define 
+// a interface and define a method that can be use by all the inherited child classes 
+
